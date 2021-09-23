c93a77b9da6d485cfae802cad29ee71448bb3fcd
KonstantinKlepikov <oformleno@gmail.com>
2021-09-23 15:49:14 +0200

    LuxAi-6, game state matrice

--- simple/base_action.py
+++ simple/base_action.py
@@ -6,6 +6,8 @@ class Geometry:
     
     """Game state maping in 3-dimmensional array
     
+    Array is orgnized as (x, y, feature vector)
+    
     0. resource {True: 1, False: 0}
     1. type of resource {None: 0, 'wood': 1, 'coal': 2, 'uranium': 3}
     2. resource amout
@@ -15,12 +17,24 @@ class Geometry:
     6. city team
     7. city cooldown
     8. can act {True: 1, False: 0}
+    9. city id
+    10. city light upkeep
+    11. is unit
+    12. unit id
+    13. unit team
+    14. unit cooldown
+    15. cargo wood
+    16. cargo coal
+    17. cargo uranium
+    18. cargo space left
+    19. can build
+    20. can act
     
     """
     
     resources = {'wood': 1, 'coal': 2, 'uranium': 3}
     teams = {0: 1, 1: 2}
-    feature_lenght = 19
+    feature_lenght = 21
 
 
     def __init__(self, game_state: Game) -> None:
@@ -28,52 +42,52 @@ class Geometry:
         self.game_state = game_state
         self.height = game_state.map.height
         self.width = game_state.map.width
+        self.game_state_massive = None
+        self.cityes = {**self.game_state.players[0].cities, **self.game_state.players[1].cities}
+        self.units = self.game_state.players[0].units + self.game_state.players[1].units
+
+    def set_map_state(self) -> None:
+        
         self.game_state_massive = np.zeros([self.height, self.width, self.feature_lenght], np.float16)
-
-
-    def set_resource(self) -> None:
         
         for h in range(self.height):
             for w in range(self.width):
                 
-                cell = self.game_state.map.get_cell(h, w)
-                cityes = {**self.game_state.players[0].cities, **self.game_state.players[1].cities}
-                empty = set(range(self.feature_lenght))
-                nonempty = []
+                cell = self.game_state.map.get_cell(w, h)
                 
                 if cell.has_resource():
-                    self.game_state_massive[h, w, 0] = 1
-                    self.game_state_massive[h, w, 1] = self.resources[cell.resource.type]
-                    self.game_state_massive[h, w, 2] = cell.resource.amount
-                    nonempty = nonempty + [0, 1, 2,]
+                    self.game_state_massive[w, h, 0] = 1
+                    self.game_state_massive[w, h, 1] = self.resources[cell.resource.type]
+                    self.game_state_massive[w, h, 2] = cell.resource.amount
                     
                 if cell.road:
-                    self.game_state_massive[h, w, 3] = cell.road
-                    nonempty = nonempty + [3,]
+                    self.game_state_massive[w, h, 3] = cell.road
                     
                 if cell.citytile:
-                    self.game_state_massive[h, w, 4] = 1
-                    self.game_state_massive[h, w, 5] = int(cell.citytile.cityid[2:])
-                    self.game_state_massive[h, w, 6] = self.teams[cell.citytile.team]
-                    self.game_state_massive[h, w, 7] = cell.citytile.cooldown
-                    nonempty = nonempty + [4, 5, 6, 7,]
+                    self.game_state_massive[w, h, 4] = 1
+                    self.game_state_massive[w, h, 5] = int(cell.citytile.cityid[2:])
+                    self.game_state_massive[w, h, 6] = self.teams[cell.citytile.team]
+                    self.game_state_massive[w, h, 7] = cell.citytile.cooldown
                     
                     if cell.citytile.can_act():
-                        self.game_state_massive[h, w, 8] = 1
-                        nonempty = nonempty + [8,]
+                        self.game_state_massive[w, h, 8] = 1
                         
-                    if cityes.get(cell.citytile.cityid, None):
-                        self.game_state_massive[h, w, 9] = cityes[cell.citytile.cityid].fuel
-                        self.game_state_massive[h, w, 10] = cityes[cell.citytile.cityid].get_light_upkeep()
-                        nonempty = nonempty + [9, 10,]
+                    if self.cityes.get(cell.citytile.cityid, None):
+                        self.game_state_massive[w, h, 9] = self.cityes[cell.citytile.cityid].fuel
+                        self.game_state_massive[w, h, 10] = self.cityes[cell.citytile.cityid].get_light_upkeep()
 
-                    
-                empty = set(range(self.feature_lenght))
-                for i in empty.difference(set(nonempty)):
-                    self.game_state_massive[h, w, i] = 0
-                    
-                    
-        
+        for unit in self.units:
+            w, h = unit.pos.x, unit.pos.y
+            self.game_state_massive[w, h, 11] = 1
+            self.game_state_massive[w, h, 12] = int(unit.id[2:])
+            self.game_state_massive[w, h, 13] = self.teams[unit.team]
+            self.game_state_massive[w, h, 14] = unit.cooldown
+            self.game_state_massive[w, h, 15] = unit.cargo.wood
+            self.game_state_massive[w, h, 16] = unit.cargo.coal
+            self.game_state_massive[w, h, 17] = unit.cargo.uranium
+            self.game_state_massive[w, h, 18] = unit.get_cargo_space_left()
+            self.game_state_massive[w, h, 19] = unit.can_build(self.game_state.map)
+            self.game_state_massive[w, h, 20] = unit.can_act()          
 
 class UnitActions:
     pass
