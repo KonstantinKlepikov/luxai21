6a2461452680ba92d4f31c79b2f999f5019a3ccb
KonstantinKlepikov <oformleno@gmail.com>
2021-12-02 21:42:09 +0200

    LuxAi-26, bugfix random agent learning

--- evol/evol.py
+++ evol/evol.py
@@ -3,7 +3,6 @@ from kaggle_environments import evaluate
 from bots.genutil import GenConstruct
 from bots.scoring import FinalScoring
 from bots.statements import GameSpace, SubGameSpace
-from bots.utility import AD
 import agent_train
 import agent_random
 import numpy as np
@@ -13,75 +12,20 @@ from typing import List, Tuple
 from loguru import logger
 import os, time, datetime, json, random, multiprocessing
 from dotenv import load_dotenv
-from pprint import pprint
-
+import click
 
 logger.remove()
 load_dotenv(dotenv_path='shared.env')
 
-# dont forget remove seeds for real learning!!!
-# set the random seed:
-RANDOM_SEED = int(os.environ.get('RANDOM_SEED'))
-random.seed(RANDOM_SEED)
-
-# game configuration
-CONFIGURATIONS = {
-    # 'seed': RANDOM_SEED,
-    # 'rows': int(os.environ.get('ROWS')),
-    # 'columns': int(os.environ.get('COLUMNS')),
-    'loglevel': int(os.environ.get('LOGLEVEL')),
-    'annotations': bool(os.environ.get('ANNOTAYIONS'))
-    }
-NUM_EPISODES = int(os.environ.get('NUM_EPISODES'))
-
-# Constants
-NUM_OF_PROCESS = multiprocessing.cpu_count()
-# Genetic Algorithm constants
 gen_const = GenConstruct()  # get genome construction object
-REPLICATE = 18 # type of replication for genome model
-# REPLICATE = 360
-GENOME_LINE_LENGHT = gen_const.prob_len  # length of genome line
-GENOME_LENGHT = REPLICATE*GENOME_LINE_LENGHT  # length of genome
-TOURNAMENT_SIZE = int(os.environ.get('TOURNAMENT_SIZE'))
-POPULATION_SIZE = int(os.environ.get('POPULATION_SIZE'))
-MAX_GENERATIONS = int(os.environ.get('MAX_GENERATIONS'))
-P_CROSSOVER = float(os.environ.get('P_CROSSOVER'))
-INDPB_CROSSOVER = float(os.environ.get('INDPB_C'))/GENOME_LENGHT
-P_MUTATION = float(os.environ.get('P_MUTATION'))
-INDPB_MUTATION = float(os.environ.get('INDPB_M'))/GENOME_LENGHT
-HALL_OF_FAME_SIZE = int(os.environ.get('HALL_OF_FAME_SIZE'))
-
-# Space initialisation
-toolbox = base.Toolbox()
-
-toolbox.register('GetRnd10', random.randint, 0, 10)
-
-# define a single objective, maximizing fitness strategy:
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-
-# create the Individual class based on list:
-creator.create("Individual", list, fitness=creator.FitnessMax)
-
-# create the individual operator to fill up an Individual instance:
-toolbox.register(
-    "individualCreator",
-    tools.initRepeat,
-    creator.Individual,
-    toolbox.GetRnd10,
-    GENOME_LENGHT
-    )
-
-# create the population operator to generate a list of individuals:
-toolbox.register(
-    "populationCreator",
-    tools.initRepeat,
-    list,
-    toolbox.individualCreator
-    )
 
 
 # Fitness calculation
-def GameScoreFitness(individual: List[int]) -> Tuple[float]:
+def GameScoreFitness(
+    individual: List[int],
+    config: dict,
+    num_of_episodes: int,
+    agent_) -> Tuple[float]:
     """Return game statistics for evaluation criterium
 
     Args:
@@ -95,15 +39,17 @@ def GameScoreFitness(individual: List[int]) -> Tuple[float]:
     agent_train.genome = gen_const.convert_daily_genome(vector=individual)
     agent_train.subgame_space = SubGameSpace()
     agent_train.game_space = GameSpace()
+    agent_random.game_space = GameSpace()
+    agent_random.gen_const = gen_const
+    agent_random.genome = gen_const.init_daily_genome()
     rewards = evaluate(
         'lux_ai_2021',
-        [agent_train.agent, 'simple_agent'],
-        # [agent_train.agent, agent_random.agent],
-        configuration=CONFIGURATIONS,
-        num_episodes=NUM_EPISODES,
+        [agent_train.agent, agent_],
+        configuration=config,
+        num_episodes=num_of_episodes,
         debug=True
         )
-    
+
     # final scoring - is a scoring, calculated inside game for each game
     # plus final rewards, returned by game. SYou can see scoring strategies
     # in scoring.py
@@ -120,29 +66,6 @@ def GameScoreFitness(individual: List[int]) -> Tuple[float]:
         )
 
     return mean_r,
-
-
-# register evaluate function
-toolbox.register("evaluate", GameScoreFitness)
-
-# Genetic operators
-# Tournament selection with tournament size
-toolbox.register("select", tools.selTournament, tournsize=TOURNAMENT_SIZE)
-
-# Crossover:
-# toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mate", tools.cxUniform, indpb=INDPB_CROSSOVER)
-
-# Flip-bit mutation:
-# indpb: Independent probability for each attribute to be flipped
-toolbox.register(
-    "mutate",
-    tools.mutUniformInt,
-    low=0,
-    up=10,
-    indpb=INDPB_MUTATION
-    )
-
 
 # define Genetic Algorithm flow with elitism
 def eaSimpleWithElitism(
@@ -178,7 +101,7 @@ def eaSimpleWithElitism(
     record = stats.compile(population) if stats else {}
     logbook.record(gen=0, nevals=len(invalid_ind), **record)
     if verbose:
-        pprint(logbook.stream)
+        click.echo(logbook.stream)
 
     # Begin the generational process
     for gen in range(1, ngen + 1):
@@ -208,15 +131,114 @@ def eaSimpleWithElitism(
         record = stats.compile(population) if stats else {}
         logbook.record(gen=gen, nevals=len(invalid_ind), **record)
         if verbose:
-            pprint(logbook.stream)
+            click.echo(logbook.stream)
 
     return population, logbook
 
 
 # Genetic Algorithm flow:
-def main():
-
+@click.command()
+@click.option('--seed', default=42, show_default=True, 
+              type=int, help='set 0 to remove random seed')
+@click.option('--size', default=0, show_default=True, type=int, 
+              help='set 12, 16, 24 or 32')
+@click.option('--loglevel', default=0, show_default=True, type=int, 
+              help='set 0, 1, 2 or 3')
+@click.option('--annotations', is_flag=True, help='on annotation mode')
+@click.option('--episodes', 'num_of_episodes', default=10, show_default=True, 
+              type=int, help='set the number of episodes for mean metrics')
+@click.option('--agent', 'agent_', default='simple_agent', show_default=True, 
+              type=click.Choice(['simple_agent', 'random']))
+def main(seed, size, loglevel, annotations, num_of_episodes, agent_):
+    
     start = datetime.datetime.now().replace(microsecond=0)
+    
+    # Constants
+    NUM_OF_PROCESS = multiprocessing.cpu_count()
+
+    # Genetic Algorithm constants
+    REPLICATE = int(os.environ.get('REPLICATE'))
+    GENOME_LINE_LENGHT = gen_const.prob_len  # length of genome line
+    GENOME_LENGHT = REPLICATE*GENOME_LINE_LENGHT  # length of genome
+    TOURNAMENT_SIZE = int(os.environ.get('TOURNAMENT_SIZE'))
+    POPULATION_SIZE = int(os.environ.get('POPULATION_SIZE'))
+    MAX_GENERATIONS = int(os.environ.get('MAX_GENERATIONS'))
+    P_CROSSOVER = float(os.environ.get('P_CROSSOVER'))
+    INDPB_CROSSOVER = float(os.environ.get('INDPB_C'))/GENOME_LENGHT
+    P_MUTATION = float(os.environ.get('P_MUTATION'))
+    INDPB_MUTATION = float(os.environ.get('INDPB_M'))/GENOME_LENGHT
+    HALL_OF_FAME_SIZE = int(os.environ.get('HALL_OF_FAME_SIZE'))
+
+    # game config
+    config = {
+        'annotations': annotations,
+        'loglevel': int(loglevel)
+        }
+    
+    if seed:
+        random.seed(seed)
+        config['seed'] = seed
+    else:
+        seed = None
+        
+    if size in [12, 16, 24, 32]:
+        config['rows'] = size
+        config['columns'] = size
+
+    click.echo(f'Config: {config}, Opponent: {agent_}')
+
+    if agent_ == 'random':
+        agent_ = agent_random.agent
+   
+    # Space initialisation
+    toolbox = base.Toolbox()
+
+    toolbox.register('GetRnd10', random.randint, 0, 10)
+
+    # define a single objective, maximizing fitness strategy:
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+
+    # create the Individual class based on list:
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    # create the individual operator to fill up an Individual instance:
+    toolbox.register(
+        "individualCreator",
+        tools.initRepeat,
+        creator.Individual,
+        toolbox.GetRnd10,
+        GENOME_LENGHT
+        )
+
+    # create the population operator to generate a list of individuals:
+    toolbox.register(
+        "populationCreator",
+        tools.initRepeat,
+        list,
+        toolbox.individualCreator
+        )
+    
+    # register evaluate function
+    toolbox.register("evaluate", GameScoreFitness, config=config, 
+                     num_of_episodes=num_of_episodes, agent_=agent_)
+
+    # Genetic operators
+    # Tournament selection with tournament size
+    toolbox.register("select", tools.selTournament, tournsize=TOURNAMENT_SIZE)
+
+    # Crossover:
+    # toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mate", tools.cxUniform, indpb=INDPB_CROSSOVER)
+
+    # Flip-bit mutation:
+    # indpb: Independent probability for each attribute to be flipped
+    toolbox.register(
+        "mutate",
+        tools.mutUniformInt,
+        low=0,
+        up=10,
+        indpb=INDPB_MUTATION
+        )
     
     pool = multiprocessing.Pool(processes=NUM_OF_PROCESS)
     toolbox.register("map", pool.map)
@@ -248,9 +270,6 @@ def main():
     
     timestamp = time.strftime("%m-%d_%H-%M", time.gmtime())
 
-    # Hall of Fame info and best bot:
-    # pprint("Hall of Fame Individuals = ", *hof.items, sep="\n")
-    # pprint("Best Ever Individual = ", hof.items[0])
     with open(f"bots_dump/best_bot_{timestamp}.json", "w") as f:
         json.dump(hof.items[0], f)
         
@@ -273,12 +292,12 @@ def main():
     plt.plot(meanFitnessValues, color='green')
     plt.xlabel('Generation')
     plt.ylabel('Max / Average Fitness')
-    plt.title(f'NUM_EPISODES: {NUM_EPISODES}, TOURNAMENT_SIZE: {TOURNAMENT_SIZE}\n\
+    plt.title(f'NUM_EPISODES: {num_of_episodes}, TOURNAMENT_SIZE: {TOURNAMENT_SIZE}\n\
 POPULATION_SIZE: {POPULATION_SIZE}, MAX_GENERATIONS: {MAX_GENERATIONS}\n\
 P_CROSSOVER: {P_CROSSOVER}, INDPB_CROSSOVER: {INDPB_CROSSOVER}\n\
 P_MUTATION: {P_MUTATION}, INDPB_MUTATION: {INDPB_MUTATION}\n\
-HALL_OF_FAME_SIZE: {HALL_OF_FAME_SIZE}, RANDOM_SEED: {RANDOM_SEED}\n\
-GENOME_LENGHT: {GENOME_LENGHT}, Time cumulative: {cum}')
+HALL_OF_FAME_SIZE: {HALL_OF_FAME_SIZE}, RANDOM_SEED: {seed}\n\
+GENOME_LENGHT: {GENOME_LENGHT}, Time cumulative: {cum}, date: {timestamp}')
 
     plt.savefig(f'img/evolution_{timestamp}.png')
 
